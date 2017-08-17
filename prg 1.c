38dc76366bcb758101d0b20d9a69b5e3865718e8
sansimech <30952869+sansimech@users.noreply.github.com>
2017-08-17 17:03:05 +0530

    c progaram
    possitive or nagative

---
+++ prg 1.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+#include<conio.g>
+voidmain()
+{
+int a;
+printf("\nEnter a number");
+scanf("%dm,&a");
+if(n>0);
+printf("\n the numer is possitive");
+else if(n<0);
+printf("\n the numer is nagative);
+else if(a==0);
+prinf("\n the number is zero"0);
+getch();
+}
